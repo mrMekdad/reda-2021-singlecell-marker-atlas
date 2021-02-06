1e57ea0ad284921ac28ac630e07fec5a11c5b9ff
mrMekdad <mekdadr6@gmail.com>
2021-02-06 18:47:00 +0100

    singlecell-marker-atlas: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2021-singlecell-marker-atlas",
+    version="0.1.0",
+    description="Marker reference organizer with metadata validation and export views.",
+    author="Red@",
+    packages=["singlecell_marker_atlas"],
+    package_dir={"": "src"},
+)
